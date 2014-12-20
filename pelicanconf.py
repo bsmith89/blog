77d6c3e52a2f10af89cb25e30b718f4986ab8e64
Byron Smith <bsmith89@gmail.com>
2014-12-20 17:41:27 -0500

    Add sitemap, documentation in conf.

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,12 +8,16 @@ SITENAME = u'Deep Ecology'
 SITESUBTITLE = u'A blog of the new microbiology.'
 SITEURL = u'http://blog.byronjsmith.com'
 
+# GOOGLE_ANALYTICS = "GOOGLE_ANALYTICS_TEST"
+# DISQUS_SITENAME = "DISQUS_TEST"
+
 # Extract metadata from the filename
 FILENAME_METADATA = '(?P<slug>.*)'
 
 TIMEZONE = 'US/Eastern'
 
 DEFAULT_LANG = u'en'
+DEFAULT_DATE = 'fs'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -38,19 +42,38 @@ GITHUB_POSITION = 'right'
 DEFAULT_PAGINATION = 10
 DEFAULT_ORPHANS = 5
 
-# Uncomment following line if you want document-relative URLs when developing
+# Document-relative URLs when developing.
+# This must be set to False for publishing.
+# See publishconf.py
 RELATIVE_URLS = True
 
-STATIC_PATHS = ['images', 'extra']
+# TODO: Consider moving pages to subdirectories based on category.
+
+STATIC_PATHS = ['images', 'extra', 'files']
 EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'}}
 
 THEME = 'theme/'
 PLUGIN_PATHS = glob("plugins/*/")
 PLUGINS=[]
 
-# Cached content is also recompiled.
-# Good for testing plugin changes.
-LOAD_CONTENT_CACHE = False
+PLUGINS.append('sitemap')
+SITEMAP = {
+        'format': 'xml',
+        'priorities': {
+            'articles': 0.6,
+            'indexes': 0.5,
+            'pages': 0.7,
+            },
+        'changefreqs': {
+            'articles': 'monthly',
+            'pages': 'monthly',
+            'indexes': 'daily',
+            },
+        }
+
+## Cached content is also recompiled.
+## Good for testing plugin changes.
+# LOAD_CONTENT_CACHE = False
 
 MATHJAX = True
 
